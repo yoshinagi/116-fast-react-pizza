74fe2b577b9de8264eddade5f91ef270782f2d4c
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-07-15 14:56:47 +0900

    add styles with Tailwind CSS

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
-import { Link } from 'react-router-dom';
-import SearchOrder from '../features/order/SearchOrder';
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="flex items-center justify-between bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Jonas</p>
+      <Username />
     </header>
   );
 }
